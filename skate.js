28965c313dffbdcc5a9d58eb9a5ab3b203ae33c3
Gabriel Leão <gabrielleao606@gmail.com>
2026-05-25 08:28:27 -0300

    Add files via upload

---
+++ skate.js
@@ -0,0 +1,18 @@
+const slideIndices = {
+    mySlides1: 1
+};
+function showSlides(slideClass){
+    const slides = document.getElementsByClassName(slideClass);
+    if(slideIndices[slideClass] > slides.length){
+        slideIndices[slideClass] = 1;
+    }
+    for(let i = 0; i < slides.length; i++){
+        slides[i].style.display = "none";
+    }
+    slides[slideIndices[slideClass] - 1].style.display = "block";
+}
+function changeSlide(n){
+    slideIndices.mySlides1 += n;
+    showSlides("mySlides1");
+}
+showSlides("mySlides1");
